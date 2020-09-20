471ffabe2b0441836b7ce7d1e1ca86d4726972fe
Hayden Watts <hayden.watts98@yahoo.com>
2020-09-20 20:25:31 +1000

    format to largest word

--- word_occurrences.py
+++ word_occurrences.py
@@ -8,5 +8,6 @@ for x in words:
     word_to_count[x] = amount + 1
 words = list(word_to_count.keys())
 words.sort()
+max_length = max((len(word) for word in words))
 for word in words:
-    print("{} : {}".format(word, word_to_count[word]))
+    print("{:{}} : {}".format(word, max_length, word_to_count[word]))
