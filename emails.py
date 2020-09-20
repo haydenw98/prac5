07ff6f4b13447c53a0611692dcbc9de21564763b
Hayden Watts <hayden.watts98@yahoo.com>
2020-09-20 20:47:40 +1000

    initial commit

--- emails.py
+++ emails.py
@@ -3,7 +3,15 @@ def main():
     email = input("Email: ")
     while email != "":
         name = get_name(email)
+        confirm = (input("Is your name {}?".format(name)).upper())
+
+        if confirm != 'Y' and confirm != '':
+            name = input("Name: ")
+        email_to_name[email] = name
         email = input("Email: ")
+
+    for k, v in email_to_name.items():
+        print("{} ({})".format(v, k))
 
 
 def get_name(email):
